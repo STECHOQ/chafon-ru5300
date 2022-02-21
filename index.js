1c957df3fe98097de0911fdc8236e4f9588fb7e4
iqrok-lenovo <blogo3x@gmail.com>
2022-02-21 14:21:02 +0700

    Chaged config param into object

--- index.js
+++ index.js
@@ -5,39 +5,57 @@ const net = require('net');
 const data = [0xC3, 0x55, 0x00, 0x01, 0x00, 0x00, 0x1A, 0x01, 0x02, 0x31, 0x80, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x1E, 0x0A, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-//~ client.connect(60000, '192.168.85.244', function() {
-const chafon = new _chafon(0x00, '192.168.85.244', 60000);
+const chafon = new _chafon({
+			addr: 0x00,
+			ip: '192.168.85.244',
+			port: 60000
+		});
+
+const {CONSTANTS} = chafon;
 
 chafon.on('data', received => {
-	//~ console.dir(received, {depth:null});
+	console.dir(received, {depth:null});
 });
 
 chafon.connect();
 
 (async () => {
-	await chafon.setActiveReading(false);
+	//~ console.log(CONSTANTS);
+	await chafon.setActiveReading(CONSTANTS.ACTIVE_READING_ON);
+
+	const setBeep = await chafon.setDeviceOneParam(
+			CONSTANTS.ADDR.BEEP,
+			CONSTANTS.BEEP_DISABLE
+		);
+	console.log(setBeep);
+
+	const setToRJ45 = await chafon.setDeviceOneParam(
+			CONSTANTS.ADDR.TRANSPORT,
+			CONSTANTS.T_RJ45
+		);
+	console.log(setToRJ45);
 
 	const system = await chafon.readSystemParam();
 	const device = await chafon.readDeviceParam();
 
-	//~ console.log(system, device);
+	console.log(system, device);
 
 	console.log(await chafon.readTagData({
-		section: chafon.CONSTANTS.section.val.USER,
+		section: CONSTANTS.SC_USER,
 		start: 0x02,
 		length: 0x08,
 		password: [0x00, 0x00,0x00, 0x00],
 	}));
 
-	//~ console.log(await chafon.writeTagData({
-		//~ section: chafon.CONSTANTS.section.val.USER,
-		//~ start: 0x02,
-		//~ data: [1,2,3,4,5,6,7,8],
-		//~ password: [0x00, 0x00,0x00, 0x00],
-	//~ }));
+	console.log(await chafon.writeTagData({
+		section: CONSTANTS.SC_USER,
+		start: 0x02,
+		data: [1,2,3,4,5,6,7,8],
+		password: [0x00, 0x00,0x00, 0x00],
+	}));
 
 	console.log(await chafon.readTagData({
-		section: chafon.CONSTANTS.section.val.USER,
+		section: CONSTANTS.SC_USER,
 		start: 0x02,
 		length: 0x08,
 		password: [0x00, 0x00,0x00, 0x00],
@@ -45,20 +63,8 @@ chafon.connect();
 
 	await sleep(5000);
 
-	await chafon.setActiveReading(true);
+	await chafon.setActiveReading(CONSTANTS.ACTIVE_READING_ON);
 
-	const setToRJ45 = await chafon.setDeviceOneParam(0x06, 0x00);
-	console.log(setToRJ45);
-
-	console.log(await chafon.readDeviceOneParam(0x01, 0x02));
+	//~ console.log(await chafon.readDeviceOneParam(0x01, 0x02));
 	//~ process.exit(0);
 })();
-
-//~ const buffer = chafon.createPacket(0x21, data);
-//~ const buffer = chafon.createPacket(0x00, 0x20);
-//~ console.log(buffer);
-
-//~ process.exit();
-
-//~ 53570003ffe074
-//~ 53570025FF21C355000100001A010231800000000100001E0A0F000000000000000000000000
